94b3d116f559e0c58b53932478d357ff72ffd515
Geno <geno+dev@fireorbit.de>
2021-05-01 16:07:02 +0200

    improve web/metrics: path anonym + up

--- web/metrics/main.go
+++ web/metrics/main.go
@@ -1,23 +1,59 @@
 package docs
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
 
-	// gorometheus
+	// gin-prometheus
 	"github.com/chenjiandongx/ginprom"
+
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	// db metrics
-	"gorm.io/plugin/prometheus"
+	gormPrometheus "gorm.io/plugin/prometheus"
 
 	"github.com/Kukoon/media-server/web"
 )
 
+var (
+	NAMESPACE string      = "service"
+	VERSION   string      = ""
+	UP        func() bool = func() bool {
+		return true
+	}
+)
+
 func init() {
 	web.ModuleRegister(func(r *gin.Engine, ws *web.Service) {
-		r.Use(ginprom.PromMiddleware(nil))
+		r.Use(ginprom.PromMiddleware(&ginprom.PromOpts{
+			EndpointLabelMappingFn: func(c *gin.Context) string {
+				url := c.Request.URL.Path
+				for _, p := range c.Params {
+					url = strings.Replace(url, p.Value, ":"+p.Key, 1)
+				}
+				return url
+			},
+		}))
+
+		prometheus.MustRegister(prometheus.NewGaugeFunc(
+			prometheus.GaugeOpts{
+				Namespace:   NAMESPACE,
+				Name:        "up",
+				Help:        "is current version of service running",
+				ConstLabels: prometheus.Labels{"version": VERSION},
+			},
+			func() float64 {
+				if UP() {
+					return 1
+				}
+				return 0
+			},
+		))
 
 		if ws.DB != nil {
-			ws.DB.Use(prometheus.New(prometheus.Config{
+			ws.DB.Use(gormPrometheus.New(gormPrometheus.Config{
+				DBName:          NAMESPACE,
 				RefreshInterval: 15,
 			}))
 
