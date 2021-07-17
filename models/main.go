11eba7e63ef0b2aea0c61c96a57d24b97650f0a0
Geno <geno+dev@fireorbit.de>
2021-07-17 23:14:46 +0200

    migration: split testdata of stream and recording

--- models/main.go
+++ models/main.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"dev.sum7.eu/genofire/golang-lib/database"
 	gormigrate "github.com/genofire/gormigrate/v2"
 )
@@ -8,6 +10,9 @@ import (
 var (
 	// DBTestConnection - url to database on setting up default modul test
 	DBTestConnection = "user=root password=root dbname=media_server host=localhost port=26257 sslmode=disable"
+
+	// loc - default timezone on testdata
+	loc = time.FixedZone("UTC+2", +2*60*60)
 )
 
 var migrations = []*gormigrate.Migration{}
