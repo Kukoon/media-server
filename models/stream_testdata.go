038cefec4894608d0d3c94fd19c77f4ecc3e7d3d
Geno <geno+dev@fireorbit.de>
2021-07-21 18:49:39 +0200

    models/stream: testdata - Pulsar Trio

--- models/stream_testdata.go
+++ models/stream_testdata.go
@@ -48,6 +48,11 @@ var (
 	testdataStream9Lang1    = uuid.MustParse("c6720db6-9d62-483d-a6ec-5cd1137b4dac")
 	testdataStream9Speaker1 = uuid.MustParse("e7e6eb6b-8188-4169-ae4e-8144d559a592")
 
+	// Grand Piano: Pulsar Trio
+	testdataStream10         = uuid.MustParse("f4d8d0e2-47bf-412c-a5e1-80c0f85a9e4c")
+	testdataStream10Lang1    = uuid.MustParse("619e61ea-a6de-42e3-b48f-6b5e52f8d920")
+	testdataStream10Speaker1 = uuid.MustParse("09120c00-acb6-4865-b1b5-6bf39963e47f")
+
 	/* Grand Piano: -
 	testdataStream9         = uuid.MustParse("")
 	testdataStream9Lang1    = uuid.MustParse("")
@@ -538,7 +543,7 @@ __Sie hat dem Jazzgesang neuen Atem eingehaucht und experimentiert ständig weit
 					Chat:      false,
 					Running:   true,
 					StartAt:   time.Date(2021, 7, 22, 17, 0, 0, 0, loc),
-					ListenAt:  time.Date(2021, 7, 22, 17, 9, 0, 0, loc),
+					ListenAt:  time.Date(2021, 7, 22, 17, 0, 0, 0, loc),
 					Poster:    "https://cdn.media.kukoon.de/videos/df1555f5-7046-4f7a-adcc-195b73949723/36ba6bfe-2b40-425d-8cc7-d7de5ec4b67a/poster.png",
 					Tags: []*Tag{
 						{ID: TestTagKonzertID},
@@ -584,6 +589,64 @@ Seine erste EP »Still Waters Run Deep« erschien im Februar 2020. `,
 				}
 				if err := tx.Delete(&Stream{
 					ID: testdataStream9,
+				}).Error; err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+		{
+			ID: "10-data-0030-01-stream-10",
+			Migrate: func(tx *gorm.DB) error {
+				if err := tx.Create(&Stream{
+					ID:        testdataStream10,
+					ChannelID: TestChannelID1,
+					EventID:   &TestEventID2,
+					Chat:      false,
+					Running:   true,
+					StartAt:   time.Date(2021, 7, 22, 20, 15, 0, 0, loc),
+					ListenAt:  time.Date(2021, 7, 22, 20, 15, 0, 0, loc),
+					Poster:    "https://cdn.media.kukoon.de/videos/df1555f5-7046-4f7a-adcc-195b73949723/f4d8d0e2-47bf-412c-a5e1-80c0f85a9e4c/poster.png",
+					Tags: []*Tag{
+						{ID: TestTagKonzertID},
+					},
+					Speakers: []*Speaker{
+						{
+							OwnerID: TestChannelID1,
+							ID:      testdataStream10Speaker1,
+							Name:    "Pulsar Trio",
+						},
+					},
+				}).Error; err != nil {
+					return err
+				}
+				if err := tx.Create(&StreamLang{
+					ID:       testdataStream10Lang1,
+					StreamID: testdataStream10,
+					Lang:     "de",
+					Title:    "Pulsar Trio",
+					Subtitle: "Donnerstag ab 20:30",
+					Short:    `Treibend, pulsierend, vital. Raffinierte Rhythmen, freie Improvisation und Stücke, deren Melodien das Zeug zum Ohrwurm haben `,
+					Long:     `Treibend, pulsierend, vital. Raffinierte Rhythmen, freie Improvisation und Stücke, deren Melodien das Zeug zum Ohrwurm haben – mit scheinbar so gegensätzlichen Instrumenten wie Sitar, Piano und Drums lässt das **Pulsar Trio** nicht nur einen neuartigen Klangraum entstehen, sondern auch eine groovige Fusion aus freiem Jazzdenken und originären Worldbeats. »Trotz ihrer vielen Taktwechsel, Breaks und Generalpausen wirkt [die Musik] nie verkopft oder kompliziert, sondern bleibt stets nachvollziehbar und zugänglich, vielfach – dank eines ausgeprägten Gespürs für griffige Melodiebögen – sogar geradezu eingängig.« (Harry Schmidt) Die creole – Preisträger konnten die hohe Qualität ihrer musikalischen Entdeckungsreise auf einer Vielzahl von Konzerten unter Beweis stellen. So gastierten sie u.a. auf den Leverkusener Jazztagen, dem Fusion-Festival, dem Glastonbury Festival, dem Rudolstadt Festival sowie dem Jazzfestival Izmir. Mit ihrem 3. Album »Zoo of Songs«, das im April 2018 bei t3 records erschien, erschließen sich die drei Musiker noch einmal ganz neue musikalische Sphären – kontemplativ, elektronisch, rhythmisch listig, energiegeladen und in satten Klangfarben präsentiert sich das Trio herausragend gereift und mit diesem pointierten Freigeist, der es so unverwechselbar macht. Die »Zoo of Songs« – Record-Release-Tour lässt sie ab 2018 auf internationalen Konzertbühnen unterwegs sein.`,
+				}).Error; err != nil {
+					return err
+				}
+				return nil
+			},
+			Rollback: func(tx *gorm.DB) error {
+				if err := tx.Delete(&StreamLang{
+					ID: testdataStream10Lang1,
+				}).Error; err != nil {
+					return err
+				}
+				if err := tx.Exec("DELETE FROM stream_speakers WHERE stream_id = ?", testdataStream10).Error; err != nil {
+					return err
+				}
+				if err := tx.Exec("DELETE FROM stream_tags WHERE stream_id = ?", testdataStream10).Error; err != nil {
+					return err
+				}
+				if err := tx.Delete(&Stream{
+					ID: testdataStream10,
 				}).Error; err != nil {
 					return err
 				}
