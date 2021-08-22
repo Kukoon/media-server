d6fc751074672b0ec1559453e20927bf31b5d12f
Geno <geno+dev@fireorbit.de>
2021-08-22 18:06:10 +0200

    admin/Channels: show restream streamkey / secret

--- web/api/channel/restream_models.go
+++ web/api/channel/restream_models.go
@@ -8,10 +8,11 @@ import (
 
 // Restream data
 type Restream struct {
-	ID       string `json:"id" example:"d592d376-0d93-4c2a-8156-5ea872fa748a Blub"`
-	Name     string `json:"name" example:"Blub"`
-	Protocol string `json:"protocol" example:"rtmp"`
-	URL      string `json:"url" example:"rtmp://a.rtmp.youtube.com/live2"`
+	ID        string `json:"id" example:"d592d376-0d93-4c2a-8156-5ea872fa748a Blub"`
+	Name      string `json:"name" example:"Blub"`
+	Protocol  string `json:"protocol" example:"rtmp"`
+	URL       string `json:"url" example:"rtmp://a.rtmp.youtube.com/live2"`
+	StreamKey string `json:"secret" example:"SUPERSECRET"`
 	// extras
 	State    string `json:"state" example:"ready"`
 	Sequence int    `json:"sequence" example:"1"`
@@ -23,12 +24,13 @@ func RestreamFromOven(data *oven.ResponsePushData) *Restream {
 		name = name[37:]
 	}
 	return &Restream{
-		ID:       data.ID,
-		Name:     name,
-		Protocol: data.Protocol,
-		URL:      data.URL,
-		State:    data.State,
-		Sequence: data.Sequence,
+		ID:        data.ID,
+		Name:      name,
+		Protocol:  data.Protocol,
+		URL:       data.URL,
+		StreamKey: data.StreamKey,
+		State:     data.State,
+		Sequence:  data.Sequence,
 	}
 }
 
